d9fa0e9f38bf91c6b84fb27cd160538303550de1
renato duriez <renato.duriez@gmail.com>
2017-03-31 16:18:07 +0200

    new page and link to these pages

--- cigard.php
+++ cigard.php
@@ -48,51 +48,10 @@ include_once 'function/autoload.php';
     </div><!-- /.container-fluid -->
     </nav>
     <br>
-            <!--Slider-->
-    <div class="container">
-    <div id="myCarousel" class="carousel slide" data-ride="carousel">
-    <!-- Indicators -->
-    <ol class="carousel-indicators">
-        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-        <li data-target="#myCarousel" data-slide-to="1"></li>
-        <li data-target="#myCarousel" data-slide-to="2"></li>
-        <li data-target="#myCarousel" data-slide-to="3"></li>
-    </ol>
-
-    <!-- Wrapper for slides -->
-    <div class="carousel-inner" role="listbox">
-        <div class="item active">
-        <img src="img_chania.jpg" alt="Chania">
-        </div>
-
-        <div class="item">
-        <img src="img_chania2.jpg" alt="Chania">
-        </div>
-
-        <div class="item">
-        <img src="img_flower.jpg" alt="Flower">
-        </div>
-
-        <div class="item">
-        <img src="img_flower2.jpg" alt="Flower">
-        </div>
-    </div>
-
-    <!-- Left and right controls -->
-    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-    </a>
-    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-    </a>
-    </div>
-    </div>
     <br>
     <div class="container">
         <?php
-            foreach (getArticles() as $id => $article) {
+            foreach (getArticlesCategory(1) as $id => $article) {
                 if($id == 0){
                     echo "<div class='row'>";
                     echo "<div class='col-md-4'>";
