1f5ff22d474343fbda10e75fb5fd34d5022eeb93
renato duriez <renato.duriez@gmail.com>
2017-03-31 11:40:34 +0200

    Create navbar

--- index.php
+++ index.php
@@ -12,6 +12,42 @@ include_once 'config.php';
     <title>whiskyLand</title>
 </head>
 <body>
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Brand</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li><a href="#">Whisky</a></li>
+        <li><a href="#">Cigards</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">Link</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Mon compte <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Mon profil</a></li>            
+            <li><a href="#">Mes commandes</a></li>
+            <li><a href="#">Historique d'achats</a></li>
+            <li><a href="#">Liste de souhaits</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Mon panier</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
     <script src="js/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/jquery.validate.min.js"></script>
