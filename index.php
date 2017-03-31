edf91a469ba25b1083de2d9fa228ee51d7f810d2
renato duriez <renato.duriez@gmail.com>
2017-03-31 11:22:55 +0200

    css link and js script

--- index.php
+++ index.php
@@ -7,9 +7,14 @@ include_once 'config.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
     <title>whiskyLand</title>
 </head>
 <body>
-    
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.validate.min.js"></script>
+    <script src="js/app.js"></script>
 </body>
 </html>
