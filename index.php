e1c1ca29316563d015e75b73042895bc2bd60eae
ryzommaster@hotmail.fr <ryzommaster@hotmail.fr>
2017-04-03 10:03:54 +0200

    bug index...

--- index.php
+++ index.php
@@ -93,69 +93,16 @@ include_once $folder.'function/autoload.php';
     <br>
     <div class="container">
         <?php
-            // foreach (getArticles() as $id => $article) {
-            //     if($id == 0){
-            //         echo "<div class='row'>";
-            //         echo "<div class='col-md-4'>";
-            //         echo "<img class='img-responsive' src=".$article->picture." >";
-            //         echo "<h3>".$article->name."</h3>";
-            //         echo "<p>".$article->price."</p>";
-            //         echo "</div>";
-            //     }
-            //     elseif ($id == 3 || $id == 6) {
-            //         echo "</div>";
-            //         echo "<div class='row'>";
-            //         echo "<div class='col-md-4'>";
-            //         echo "<img class='img-responsive' src=".$article->picture." >";
-            //         echo "<h3>".$article->name."</h3>";
-            //         echo "<p>".$article->price."</p>";
-            //         echo "</div>";
-            //     }
-            //     else{
-            //         echo "<div class='col-md-4'>";
-            //         echo "<img class='img-responsive' src=".$article->picture." >";
-            //         echo "<h3>".$article->name."</h3>";
-            //         echo "<p>".$article->price."</p>";
-            //         echo "</div>";
-            //     }
-            // }
-            // echo "------------------------WHISKY------------------";
-            // foreach (getArticlesCategory(0) as $id => $article) {
-            //     if($id == 0){
-            //         echo "<div class='row'>";
-            //         echo "<div class='col-md-4'>";
-            //         echo "<img class='img-responsive' src=".$article->picture." >";
-            //         echo "<h3>".$article->name."</h3>";
-            //         echo "<p>".$article->price."</p>";
-            //         echo "</div>";
-            //     }
-            //     elseif ($id == 3 || $id == 6) {
-            //         echo "</div>";
-            //         echo "<div class='row'>";
-            //         echo "<div class='col-md-4'>";
-            //         echo "<img class='img-responsive' src=".$article->picture." >";
-            //         echo "<h3>".$article->name."</h3>";
-            //         echo "<p>".$article->price."</p>";
-            //         echo "</div>";
-            //     }
-            //     else{
-            //         echo "<div class='col-md-4'>";
-            //         echo "<img class='img-responsive' src=".$article->picture." >";
-            //         echo "<h3>".$article->name."</h3>";
-            //         echo "<p>".$article->price."</p>";
-            //         echo "</div>";
-            //     }
-            // }
-            echo "------------------------CIGARD------------------";
-            foreach (getArticlesCategory(1) as $id => $article) {
+            foreach (getArticles() as $id => $article) {
                 if($id == 0){
                     echo "<div class='row'>";
                     echo "<div class='col-md-4'>";
                     echo "<img class='img-responsive thumbnail' src=".$article->picture." >";
-                    echo "<h3>".$article->name." "."<strong>".$article->price." €</strong>"."</h3>";
+                    echo "<h3>".$article->name."</h3>";
+                    echo "<p>".$article->price."</p>";
                     echo "</div>";
                 }
-                elseif ($id == 3 || $id == 6 || $id == 9 || $id == 12) {
+                elseif ($id == 3 || $id == 6) {
                     echo "</div>";
                     echo "<div class='row'>";
                     echo "<div class='col-md-4'>";
@@ -167,7 +114,7 @@ include_once $folder.'function/autoload.php';
                     echo "<div class='col-md-4'>";
                     echo "<img class='img-responsive thumbnail' src=".$article->picture." >";
                     echo "<h3>".$article->name." "."<strong>".$article->price." €</strong>"."</h3>";
-                    echo "</div>";
+					echo "</div>";
                 }
             }
         ?>
