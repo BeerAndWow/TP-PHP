9683ccc2591022d00caaff2335b356527922861b
renato duriez <renato.duriez@gmail.com>
2017-04-03 11:48:09 +0200

    new fkcing bdd

--- sql/tp_php.sql
+++ sql/tp_php.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Lun 03 Avril 2017 à 09:48
+-- Généré le :  Lun 03 Avril 2017 à 11:47
 -- Version du serveur :  10.1.19-MariaDB
 -- Version de PHP :  7.0.13
 
@@ -44,13 +44,18 @@ INSERT INTO `articles` (`id`, `name`, `content`, `price`, `picture`, `stock`, `i
 (1, 'The first édition', 'Whiskay is love, Whiskay is life !', 42, 'img/articles/article1.jpg', 2, 0),
 (2, 'Auchentochan', 'Whiskay qsdis love, Whiskagrgrgdgrgy is life !', 32, 'img/articles/article2.jpg', 2, 0),
 (3, 'The BenRiach', 'Whiskayqqqqqqqq is love, Whiskaddddddy is life !', 52, 'img/articles/article5.jpg', 2, 0),
-(4, 'Cigard', 'Cignord is love, Cignord is life !', 12, 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTsBpKOZWyQVR6Hhz68q4TYRJhOVQMTd4suP_zGpkTOVrpikYyQ-NuK6A', 2, 1),
 (5, 'Glenfarclas', '', 86, 'img/articles/article7.jpg', 3, 0),
 (6, 'Glenfiddich 18 ans', '', 86, 'img/articles/article9.jpg', 3, 0),
 (7, 'Glenfiddich', '', 86, 'img/articles/article10.jpg', 3, 0),
 (8, 'IPA Experiment', '', 86, 'img/articles/article11.jpg', 3, 0),
 (9, 'Nikka', '', 86, 'img/articles/article13.jpg', 3, 0),
-(10, 'Penderyn', '', 86, 'img/articles/article16.jpg', 3, 0);
+(10, 'Penderyn', '', 86, 'img/articles/article16.jpg', 3, 0),
+(11, 'cigard', 'azeazeeazeaeaeaeeazea', 9, 'img/cigar/cigar2.jpg', 20, 1),
+(12, 'cigard', 'azeazeeazeaeaeaeeazea', 9, 'img/cigar/cigar3.jpg', 18, 1),
+(13, 'cigard', 'azeazeeazeaeaeaeeazea', 9, 'img/cigar/cigar4.jpg', 27, 1),
+(14, 'cigard', 'azeazeeazeaeaeaeeazea', 18, 'img/cigar/cigar5.jpg', 8, 1),
+(15, 'cigard', 'azeazeeazeaeaeaeeazea', 32, 'img/cigar/cigar6.jpg', 2, 1),
+(16, 'cigard', 'azezaeeaaeaeaz', 11, 'img/cigar/cigar1.jpg', 45, 1);
 
 -- --------------------------------------------------------
 
@@ -110,7 +115,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT pour la table `articles`
 --
 ALTER TABLE `articles`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 --
 -- AUTO_INCREMENT pour la table `category`
 --
