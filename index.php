9a609a7c90a175a2a6c7534ea1c83dd0b2a5bc86
renato duriez <renato.duriez@gmail.com>
2017-04-03 10:33:12 +0200

    fix favicon and slider

--- index.php
+++ index.php
@@ -70,55 +70,51 @@ include_once $folder.'function/autoload.php';
     <!-- Wrapper for slides -->
     <div class="carousel-inner" role="listbox">
         <div class="item active">
-        <img src="img_chania.jpg" alt="Chania">
+        <img src="img/slider/Slider1.jpg" alt="Chania">
         </div>
 
         <div class="item">
-        <img src="img_chania2.jpg" alt="Chania">
+        <img src="img/slider/Slider2.jpg" alt="Flower">
         </div>
 
         <div class="item">
-        <img src="img_flower.jpg" alt="Flower">
+        <img src="img/slider/Slider3.jpg" alt="Flower">
         </div>
 
         <div class="item">
-        <img src="img_flower2.jpg" alt="Flower">
+        <img src="img/slider/Slider4.jpg" alt="Flower">
         </div>
 
         <div class="item">
-        <img src="img_flower2.jpg" alt="Flower">
+        <img src="img/slider/Slider5.jpg" alt="Flower">
         </div>
 
         <div class="item">
-        <img src="img_flower2.jpg" alt="Flower">
+        <img src="img/slider/Slider6.jpg" alt="Flower">
         </div>
 
         <div class="item">
-        <img src="img_flower2.jpg" alt="Flower">
+        <img src="img/slider/Slider7.jpg" alt="Flower">
         </div>
 
         <div class="item">
-        <img src="img_flower2.jpg" alt="Flower">
+        <img src="img/slider/Slider8.jpg" alt="Flower">
         </div>
 
         <div class="item">
-        <img src="img_flower2.jpg" alt="Flower">
-        </div>
-
-        <div class="item">
-        <img src="img_flower2.jpg" alt="Flower">
+        <img src="img/slider/Slider9.jpg" alt="Flower">
         </div>
     </div>
 
     <!-- Left and right controls -->
-    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+    <!--<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
         <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
         <span class="sr-only">Previous</span>
     </a>
     <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
         <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
         <span class="sr-only">Next</span>
-    </a>
+    </a>-->
     </div>
     </div>
     <br>
